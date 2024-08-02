280a3eff1ee72dc963b60deecaba3d3c32cc53ef
SudhakaranV17 <44158979+SudhakaranV17@users.noreply.github.com>
2024-08-02 22:52:17 +0530

    mmax

--- includes/parts/footer.php
+++ includes/parts/footer.php
@@ -1,3 +1,34 @@
+<footer class="footer" id="footer">
+    <div class="footer-main">
+        <div class="footer-main-contact">
+            <div class="footer-main-contact-logo">
+                <img src="" alt="">
+            </div>
+            <div class="footer-main-contact-addr">
+                <p>lorem ipsum dolor<br>lorem ipsum</p>
+            </div>
+            <div class="footer-main-contact-credits">
+                <p>All rights reserved</p>
+            </div>
+        </div>
+        <div class="footer-main-content">
+            <div class="footer-main-content-parallax">
+                <div class="footer-main-content-parallax-img">
+                    <img src="../../assests/images/casa/exp1.jpg" alt="">
+                </div>
+                <div class="footer-main-content-parallax-social">
+                    <img src="" alt="">
+                    <img src="" alt="">
+                    <img src="" alt="">
+                </div>
+            </div>
+            <div class="footer-main-content-details"></div>
+        </div>
+    </div>
+</footer>
+
+
+
 <script src="https://accounts.google.com/gsi/client" async></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script src="https://apis.google.com/js/platform.js" async defer></script>
